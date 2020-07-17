b2f58fb524bad780d713a7b886d78fc5cb7c51f3
Roi <vosroix@gmail.com>
2020-07-17 16:21:02 +0500

    added a decorator

--- projects/decorator.js
+++ projects/decorator.js
@@ -7,18 +7,30 @@ let fac = function f(n) {
 function logResultDecorator (func, funcName) {
 
   
-  return function () {
+  return function (...args) {
 
-    let result = func.apply(this, arguments);
+    let result = func(args);
     console.log(`Результат функции ${funcName} : ${result}`);
-    return result;
-
   } 
 }
 
 
+function callCountDecorator (func, funcName) {
+  let count = 0;
+  return function (...args) {
+    count++;
+    func(args);
+    console.log(`Функиция ${funcName} была вызванна ${count} раз`);
+  }
+}
+
 
 
 fac = logResultDecorator(fac, 'factorial');
+fac = callCountDecorator(fac, 'factorial');
+
+console.log(fac);
 
 fac(5);
+fac(6);
+fac(7);
--- test.js
+++ test.js
@@ -1,22 +1,36 @@
-function sum(a) {
 
-  let currentSum = a;
+let fac = function f(n) {
+  return (n !== 1) ? f(n - 1) * n : n;
+}
 
-  function f(b) {
-    currentSum += b;
-    return f;
+
+function logResultDecorator (func, funcName) {
+
+  
+  return function (...args) {
+
+    let result = func(args);
+    console.log(`Результат функции ${funcName} : ${result}`);
+  } 
+}
+
+
+function callCountDecorator (func, funcName) {
+  let count = 0;
+  return function (...args) {
+    count++;
+    func(args);
+    console.log(`Функиция ${funcName} была вызванна ${count} раз`);
   }
-
-  f.toString = function() {
-    return currentSum;
-  };
-
-  return f;
 }
 
 
 
+fac = logResultDecorator(fac, 'factorial');
+fac = callCountDecorator(fac, 'factorial');
 
+console.log(fac);
 
-console.log(sum(1)(2))
-
+fac(5);
+fac(6);
+fac(7);
