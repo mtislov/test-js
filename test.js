1fd00bfdea2dc485164b6bdfdbead76ad722dec9
Max <49519905+Roixxx@users.noreply.github.com>
2020-08-20 13:54:54 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,75 +1,64 @@
 
-let fac = function f(n) {
-  return (n !== 1) ? f(n - 1) * n : n;
+
+function menStillStanding(cards) {
+
 }
+  
+  
+  
+
+/// ПЕРЕДЕЛАТЬ СКРИПТ БЕЗ СОРТИРОВКИ
+
+/*
 
 
-
-function logResultDecorator (func, funcName) {
-
-  return function (...args) {
-
-    let result = func(args);
-    console.log(`Результат функции ${funcName} : ${result}`);
-    return result;
-  } 
-}
-
-function cacheDecorator(func) {
-  let cache = {};
-  return function(...args) {
-    if (typeof cache[args] === 'undefined') {
-      cache[args] = func(args);
-    } else {
-      console.log('Результат функции взят из кэша: ' + cache[args]);
+function menStillStanding(cards) {
+    let teamA = {
+        men: 11,
+        redCards: {},
+        yellowCards: {},
     }
+
+    let teamB = {
+        men: 11,
+        redCards: {},
+        yellowCards: {},
+    }
+    
+    for (let i = 0; i <= teamA.men; i++) {
+      teamA.yellowCards[i] = 0;
+      teamB.yellowCards[i] = 0;
+    }
+    
+    console.log(cards)
+    
+    if (!cards[0]) return [teamA.men, teamB.men];
+  
+  
+    cards.forEach((card, i) => {
+  
+      let team = card[0] == 'A' ? teamA : teamB;
+      let player = card.slice(1, -1);
+      let cardColor = card.slice(-1);
+  
+      if (teamA.men < 7 || teamA.men < 7) return [teamA.men, teamB.men];
+  
+      if (cardColor == 'R') {
+        team.men--;
+      }
+      
+      if (cardColor == 'Y') {
+        team.yellowCards[player]++;
+  
+        if (team.yellowCards[player] == 2) {
+          delete team.yellowCards[player];
+          team.men--;
+        }
+      }
+    });
+  
+    console.log( [teamA.men, teamB.men])
+    return [teamA.men, teamB.men];
   }
-}
-
-
-function callCountDecorator (func, funcName) {
-  let count = 0;
-  return function (...args) {
-    count++;
-    console.log(`Функиция ${funcName} была вызванна ${count} раз`);
-    func(args);
-  }
-}
-
-function timeDecorator (func, funcName) {
-
-  return function (...args) {
-    console.time(`Время выполнения функции ${funcName}`);
-    func(args);
-    console.timeEnd(`Время выполнения функции ${funcName}`);
-  }
-}
-
-function argumentsCountDecorator(func, requiredNumber) {
- return function (...args) {
-   let argsCount = args.length;
-
-   if (requiredNumber !== argsCount) {
-     console.warn('Кол во аргументов не совпадает');
-     return;
-   }
-
-   func(args);
- }
-
-}
-
-
-
-
-fac = logResultDecorator(fac, 'factorial');
-fac = cacheDecorator(fac, 'factorial');
-fac = callCountDecorator(fac, 'factorial');
-fac = timeDecorator(fac, 'factorial');
-fac = argumentsCountDecorator(fac, 1);
-
-
-fac(12);
-fac(12);
-fac(11);
-
+  
+ */
