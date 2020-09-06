260b31cc61aa5aee5d2930a5460a0e91ca34a2d9
Max <49519905+Roixxx@users.noreply.github.com>
2020-09-06 13:08:59 +0500

    modules in js

--- test.js
+++ test.js
@@ -1,64 +1,44 @@
+// Modules / export syntaxis
 
-function menStillStanding(cards) {
+// variables
+export let a = 'a';
 
-}
-  
-  
-  
+let b = 'b';
+let c = 'c';
+export {b, c};
 
-/// ПЕРЕДЕЛАТЬ СКРИПТ БЕЗ СОРТИРОВКИ
-
-/*
-
-
-function menStillStanding(cards) {
-    let teamA = {
-        men: 11,
-        redCards: {},
-        yellowCards: {},
-    }
-
-    let teamB = {
-        men: 11,
-        redCards: {},
-        yellowCards: {},
-    }
-    
-    for (let i = 0; i <= teamA.men; i++) {
-      teamA.yellowCards[i] = 0;
-      teamB.yellowCards[i] = 0;
-    }
-    
-    console.log(cards)
-    
-    if (!cards[0]) return [teamA.men, teamB.men];
-  
-  
-    cards.forEach((card, i) => {
-  
-      let team = card[0] == 'A' ? teamA : teamB;
-      let player = card.slice(1, -1);
-      let cardColor = card.slice(-1);
-  
-      if (teamA.men < 7 || teamA.men < 7) return [teamA.men, teamB.men];
-  
-      if (cardColor == 'R') {
-        team.men--;
-      }
-      
-      if (cardColor == 'Y') {
-        team.yellowCards[player]++;
-  
-        if (team.yellowCards[player] == 2) {
-          delete team.yellowCards[player];
-          team.men--;
-        }
-      }
-    });
-  
-    console.log( [teamA.men, teamB.men])
-    return [teamA.men, teamB.men];
+// class
+export class Person {
+  constructor( ) {
+    this.name = name;
   }
-  
- */
+}
 
+// Function
+export function sayHello() {
+  console.log('Hello!')
+}
+
+/* or 
+
+  function sayHello() {
+    console.log('Hello!')
+  }
+
+  export {Person, sayHello}
+
+*/
+
+// Rename 
+export { b as varB};
+
+/////////////////////////////
+// Modules / import syntaxis
+
+import {b, c} from './file.js';
+
+// import all in 
+import * as letters from './file.js'; 
+console.log(letters.b, letters.c);  // letters is object 
+
+//default ???????????????????????????!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
